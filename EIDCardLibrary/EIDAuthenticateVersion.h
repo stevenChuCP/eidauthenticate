d437283235884f7e814c44c639a7c0c5ff2cc653
Steven Chu <stnc5120@gmail.com>
2023-02-04 15:02:13 +0800

    Add missing file from source tree

---
+++ EIDCardLibrary/EIDAuthenticateVersion.h
@@ -0,0 +1,7 @@
+//
+// The content of this file is found in a ticket from the original Sourceforge repository.
+// (https://sourceforge.net/p/eidauthenticate/bug/2/)
+//
+
+#define EIDAuthenticateVersionNumeric 0,5,0,4
+#define EIDAuthenticateVersionText "0.5.0.4"
